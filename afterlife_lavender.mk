33738d5659714acf18611baeaff82e9f6e30ae8d
Ritikk11 <144951533+Ritikk11@users.noreply.github.com>
2025-01-09 19:27:50 +0530

    Update and rename arrow_lavender.mk to afterlife_lavender.mk

---
+++ afterlife_lavender.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common  stuff
+$(call inherit-product, vendor/afterlife/config/common.mk)
+AFTERLIFE_GAPPS := true
+AFTERLIFE_CORE := true
+AFTERLIFE_MAINTAINER := @Ritikk0011
+TARGET_SUPPORTS_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := afterlife_lavender
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := lavender
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi Note 7
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := lavender
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.5.3.0.QFGCNXM release-keys"
+
+BUILD_FINGERPRINT := xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGCNXM:user/release-keys
